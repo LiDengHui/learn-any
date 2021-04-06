eba1fbdd00218f8e29e2ea2497fe6567d714ea0a
denghui.li <3379489032@qq.com>
2021-04-06 23:30:41 +0800

    ✨feat: 初始化项目

---
+++ packages/promise-all/index.spec.ts
@@ -0,0 +1,18 @@
+import { promiseAll } from "./index";
+
+describe("promise-all", () => {
+    // var promiseAll = Promise.all.bind(Promise);
+    it("should return right number", async () => {
+        const arr = [Promise.resolve(1), Promise.resolve(2)];
+
+        const list = await promiseAll(arr);
+
+        expect(list).toStrictEqual([1, 2]);
+    });
+
+    it("should return reject value", async () => {
+        const arr = [Promise.resolve(1), Promise.reject(2)];
+
+        await expect(promiseAll(arr)).rejects.toBe(2);
+    });
+});
